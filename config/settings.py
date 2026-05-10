a184df3919f7ad6d740ae7c3eaf188a4cbb7da5f
Dinoza <dilnozaumorova1999@gmail.com>
2026-05-10 21:55:14 +0500

    allowed hostga uladik

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY =os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['84.54.70.112', 'localhost', '127.0.0.1', '.onrender.com']
+ALLOWED_HOSTS = ['84.54.70.112', 'localhost', '127.0.0.1', '.onrender.com','xorazmjadid.uz']
 
 
 
